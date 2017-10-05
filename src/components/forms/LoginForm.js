3380adc07ce132e879d704ff4cd23653708da9e2
diegovilarinho <contato@diegovilarinho.com.br>
2017-10-05 01:06:16 -0300

    Login form with client-side validation

---
+++ src/components/forms/LoginForm.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Form, Button } from 'semantic-ui-react';
+import Validator from 'validator';
+import InlineError from '../messages/InlineError';
+
+class LoginForm extends Component {
+    state = {
+        data: {
+            email: '',
+            password: ''
+        },
+        loading: false,
+        errors: {}
+    };
+
+    onChange = e => 
+        this.setState({
+            data: { ...this.state.data, [e.target.name]: e.target.value }
+        });
+
+    onSubmit = () => {
+        const errors = this.validate(this.state.data);
+        this.setState({ errors });
+        if(Object.keys(errors).length === 0) {
+            this.props.submit(this.state.data);
+        }
+    };
+
+    validate = (data) => {
+        const errors = {};
+        if(!Validator.isEmail(data.email)) errors.email = "E-mail inválido";
+        if(!data.password) errors.password = "A senha não pode estar em branco";
+        return errors;
+    }
+    
+
+    render() {
+        const { data, errors } = this.state;
+
+        return (
+            <Form onSubmit={this.onSubmit}>
+                <Form.Field error={!!errors.email}>
+                    <label htmlFor="email">E-mail</label>
+                    <input 
+                        type="email" 
+                        id="email" 
+                        name="email" 
+                        placeholder="email@seuemail.com" 
+                        value={data.email}
+                        onChange={this.onChange}
+                    />
+                    {errors.email && <InlineError text={errors.email} />}
+                </Form.Field>
+                <Form.Field error={!!errors.password}>
+                    <label htmlFor="password">Senha</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name="password" 
+                        placeholder="Digite sua senha" 
+                        value={data.password}
+                        onChange={this.onChange}
+                    />
+                    {errors.password && <InlineError text={errors.password} />}
+                </Form.Field>
+                <Button primary>Login</Button>
+            </Form>
+        );
+    }
+}
+
+LoginForm.propTypes = {
+    submit: PropTypes.func.isRequired
+};
+
+export default LoginForm;
